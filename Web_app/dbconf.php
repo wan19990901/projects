60754db478d53fe20fc67ad425b434c6fc20a6b5
wan19990901 <wanguangya@gmail.com>
2019-04-03 18:59:04 -0500

    update

--- Web_app/dbconf.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $host = 'localhost';
-    $dbname = 'demo';
-    $username = 'root';
-    $password = 'Qwer12345678!';
-
-
-     ?>
--- Web_app/my_apps/google_apps/static/js/test.sql
+++
@@ -1,1 +0,0 @@
- LOAD DATA LOCAL INFILE '~/Documents/googleplaystore_user_reviews.csv'  INTO TABLE reviews  FIELDS TERMINATED BY ','  ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
